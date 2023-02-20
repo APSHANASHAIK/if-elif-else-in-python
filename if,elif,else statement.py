1053edaa849adeba8f787fb3f9b29842a629135b
Apshana <90459816+APSHANASHAIK@users.noreply.github.com>
2023-02-20 20:46:05 +0530

    Add files via upload

---
+++ if,elif,else statement.py
@@ -0,0 +1,8 @@
+print("enter the student age")
+age=int(input())
+if age>18:
+    print("the student can drive")  
+elif age==18:
+     print("we will think")     
+else:
+    print("the student cannot drive")
